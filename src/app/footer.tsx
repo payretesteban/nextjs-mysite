12f91fedb9547d62d48ae4280a10f6a606762721
payretesteban <263746736+payretesteban@users.noreply.github.com>
2026-04-16 17:49:53 -0700

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="container mx-auto max-w-3xl p-8 text-center text-slate-400 text-xs">
+    <footer className="container mx-auto max-w-3xl p-8 text-center text-xs">
       © {new Date().getFullYear()} Esteban Payret
     </footer>
   );
